9b074a8091d32770fdc4f56475f379b6711b004b
Alessandro <luisialessandro95@gmail.com>
2021-11-05 15:43:48 +0100

    prova push

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,14 @@
 alert ('Hello World');
+
+
+const firstName = prompt("What's your fucking name");
+
+const lastName = prompt("What's your fucking lastname");
+
+const color = prompt("What's your fucking favourite color");
+
+const pele = firstName + lastName + color + '21';
+
+console.log(pele);
+
+document.getElementById('passwd').innerHTML = 'la tua password è: '+ pele
